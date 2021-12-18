144fb7958aa94ac6cd384a2eaa1cf81d2048aa31
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-12-17 22:12:09 -0300

    chore: More information in init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ GREEN='\033[1;32m'
 NC='\033[0m'
 
 # Functions
+function blankLine() {
+  echo
+}
 function printHeader() {
   # shellcheck disable=SC2059
   printf "${GREEN}--- $1 ---${NC}\n"
@@ -43,22 +46,31 @@ function getExtension() {
 printHeader 'Project'
 TITLE=$(getInput 'Title' 'Title here')
 DESCRIPTION=$(getInput 'Description' 'Description here')
+blankLine
 
 printHeader 'Code Generation Target'
 TARGET_LANGUAGE=$(getInput 'Target language' 'Java')
 TARGET_EXTENSION=$(getExtension "$TARGET_LANGUAGE")
+blankLine
 
 printHeader 'Grammar'
 GRAMMAR=$(getInput 'The name of your grammar' 'Expr')
+blankLine
 
 # Get stubs and replace variables
 STUB_FOLDER='.stubs'
 
+echo "Generating README.md"
 sed "s/{{TITLE}}/${TITLE}/" "${STUB_FOLDER}/README.stub" \
   | sed "s/{{DESCRIPTION}}/${DESCRIPTION}/" > 'README.md'
+echo "Done! (README.md)"
 
+echo "Generating Makefile"
 sed "s/{{TARGET_LANGUAGE}}/${TARGET_LANGUAGE}/" "${STUB_FOLDER}/Makefile.stub" \
   | sed "s/{{TARGET_EXTENSION}}/${TARGET_EXTENSION}/" \
   | sed "s/{{GRAMMAR}}/${GRAMMAR}/" > 'Makefile'
+echo "Done! (Makefile)"
 
+echo "Generating src/${GRAMMAR}.g4"
 sed "s/{{GRAMMAR}}/${GRAMMAR}/" "${STUB_FOLDER}/Grammar.stub" > "src/${GRAMMAR}.g4"
+echo "Done! (src/${GRAMMAR}.g4)"
